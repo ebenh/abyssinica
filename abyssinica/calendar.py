93a1789c2a4c25a188445a68c65e10567f4b9452
Ebenezer Hailemariam <eben@derso.org>
2023-08-12 13:29:46 -0400

    Removing dead code

--- abyssinica/calendar.py
+++ abyssinica/calendar.py
@@ -6,27 +6,6 @@ _LEAP_YEAR_CYCLE_DAYS = 1461
 This figure can be derived from (365 * 4) + 1 or 365.25 * 4"""
 
 
-def get_julian_date(ordinal_date):
-    # ordinal_date = datetime.now().toordinal()
-    four_year_cycle = int(365.25 * 4)  # 1461
-
-    # offset = -(7 * 365)  # Julian day corresponding to Ethiopian date 1/1/1
-    offset = 0
-
-    a, b = divmod(ordinal_date + offset, four_year_cycle)
-    print(a, b)
-
-    year = (a * 4) + (b // 364)
-    day_of_year = 365 if b == 0 else b % 364
-    if day_of_year <= 360:
-        month_of_year, day_of_month = divmod(day_of_year, 30)
-    else:
-        month_of_year = 12
-        day_of_month = (day_of_year - 360)
-
-    return day_of_year + 1, month_of_year + 1, day_of_month + 1, year
-
-
 def get_leap_year_cycles(ordinal_date):
     """For a given ordinal date, this method returns the following two-tuple:\n
     - An integer representing the count of full leap year cycles that have occurred, and
@@ -61,8 +40,3 @@ def my_mod(x, k):
 
 if __name__ == '__main__':
     print(get_month(1461-5))
-    # day_of_year, month_of_year, day_of_month, year = get_julian_date(1)
-    # print(f'day_of_year {day_of_year}')
-    # print(f'month_of_year {month_of_year}')
-    # print(f'day_of_month {day_of_month}')
-    # print(f'year {year}')
